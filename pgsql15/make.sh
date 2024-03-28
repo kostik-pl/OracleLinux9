b4979a7a3c5fadd93bd02f2040da4acb716650c9
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2024-03-28 13:48:06 +0200

    Update make.sh

--- pgsql15/make.sh
+++ pgsql15/make.sh
@@ -4,3 +4,4 @@ podman images
 podman run --name pgsql15--ip 10.88.0.2 --hostname pgslq15.local -dt -p 5432:5432 -v /_data:/_data localhost/pgsql15
 podman login docker.io
 podman push localhost/pgsql15 docker.io/kostikpl/ol9:pgsql15
+podman run --name pgsql15 --ip 10.88.0.2 --hostname $HOSTNAME -dt -p 5432:5432 -v /_data:/_data docker.io/kostikpl/ol9:pgsql15
