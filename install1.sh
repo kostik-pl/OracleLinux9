796e706c0c17a691b624c6f5d9feca1713d5a352
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2024-03-29 10:43:42 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -8,6 +8,7 @@ sed -i 's/enforcing/disabled/g' /etc/selinux/config
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+firewall-cmd --permanent --zone=public --remove-service=dhcpv6-client
 
 #Setup system
 dnf upgrade -y
