de032a0cb2a47e55aa486ec2e4b6d133ad433e94
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2024-03-29 08:12:52 +0200

    Add files via upload

--- pgsql15/container-entrypoint.sh
+++ pgsql15/container-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     pg_ctl initdb -D $PGDATA -o "--locale=$LANG --lc-collate=$LANG"
-    printf "host all all all md5\n" >> $PGDATA/pg_hba.conf
+    printf "host    all             all             all                     md5\n" >> $PGDATA/pg_hba.conf
 fi
 
 exec "$@"
