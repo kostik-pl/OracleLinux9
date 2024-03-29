b1f20695241cdfe19d21f599b86b607d5cbd4a8c
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2024-03-29 08:07:40 +0200

    Add files via upload

--- pgsql15/container-entrypoint.sh
+++ pgsql15/container-entrypoint.sh
@@ -3,8 +3,7 @@
 set -e
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-    pg_ctl initdb -D $PGDATA -o "--locale=$LANG"
-    psql -c "ALTER USER postgres WITH PASSWORD 'RheujvDhfub72';"
+    pg_ctl initdb -D $PGDATA -o "--locale=$LANG --lc-collate=$LANG"
     printf "host all all all md5\n" >> $PGDATA/pg_hba.conf
 fi
 
