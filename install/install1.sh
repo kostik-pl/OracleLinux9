f0d667296d5547fae49b039d5a8f956cce3ab2a6
kostik-pl <47416257+kostik-pl@users.noreply.github.com>
2024-06-06 10:42:43 +0300

    Update install1.sh

--- install/install1.sh
+++ install/install1.sh
@@ -4,7 +4,7 @@
 sed -i 's/enforcing/disabled/g' /etc/selinux/config
 
 #Disable IP6 in GRUB or SYSTEM_CONFIG
-#grub2-editenv - set "$(grub2-editenv - list | grep kernelopts) ipv6.disable=1"
+grub2-editenv - set "$(grub2-editenv - list | grep kernelopts) ipv6.disable=1"
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sysctl -w net.ipv6.conf.lo.disable_ipv6=1
